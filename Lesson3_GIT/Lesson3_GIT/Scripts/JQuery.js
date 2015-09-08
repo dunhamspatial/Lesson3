8c35993510a5fb7f7353893b8c194cb9d8fef075
Jeffrey Dunham <dunhamspatial@gmail.com>
2015-09-08 09:41:48 -0600

    Final Version of Lesson 3

---
+++ Lesson3_GIT/Lesson3_GIT/Scripts/JQuery.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    alert('Well Hello There');
+
+    $("#map-canvas").width("800px").height("500px").gmap3({
+        marker:{address: "1930 Dover St, Lakewood CO"
+        },
+        map: {
+            options: {
+                zoom: 14
+                     }
+             }
+    });
+      
+});
+
---
+++ Lesson3_GIT/Lesson3_GIT/Scripts/JavaScript.js
@@ -0,0 +1,38 @@
+﻿//$(document).ready(function () {
+
+//    //$("#map-canvas").gmap3({
+//    //    marker: {
+//    //        address: "1930 Dover St, Lakewood CO"
+//    //    },
+//    //    map: {
+//    //        options: {
+//    //            zoom: 14
+//    //        }
+//    //    }
+//    //});
+
+//});
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize() {
+
+    var latlng = new google.maps.LatLng(39.746474, -105.092935);
+
+    var mapOptions = {
+        center: latlng,
+        scrollWheel: false,
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+
+    var marker = new google.maps.Marker({
+        position: latlng,
+        url: '/',
+        animation: google.maps.Animation.DROP
+    });
+
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    marker.setMap(map);
+
+};
